7c7e177b7e5df49194e4cbe2866c760f1a9fbeda
Adrien Ysnel <adrien.ysnel@compte-nickel.fr>
2021-09-10 15:53:33 +0200

    Exercice 1: solution

--- applications/training-api/src/main/scala/AppServer.scala
+++ applications/training-api/src/main/scala/AppServer.scala
@@ -1,5 +1,6 @@
 package fr.fpe.school
 
+import api.AccountAPI
 import config.Config
 import routes.BankRoutes
 
@@ -24,7 +25,9 @@ object AppServer extends IOApp {
 
   private def buildBankRoutes() = {
 
-    val bankRoutes = new BankRoutes()
+    val accountAPI = new AccountAPI()
+
+    val bankRoutes = new BankRoutes(accountAPI)
 
     bankRoutes.routes
 
---
+++ domain/src/main/scala/api/AccountAPI.scala
@@ -0,0 +1,7 @@
+package fr.fpe.school
+package api
+
+final class AccountAPI() {
+
+  def createAccount(name: String): String = name
+}
