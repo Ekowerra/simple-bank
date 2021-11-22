60047a560f94287169b7a9718a63889d877da15e
Nicolas SANS <nicolas.sansd@gmail.com>
2021-11-22 16:45:46 +0100

    added blank rule

--- domain/src/main/scala/api/AccountAPI.scala
+++ domain/src/main/scala/api/AccountAPI.scala
@@ -5,6 +5,5 @@ import model.Account
 
 final class AccountAPI() {
 
-  def createAccount(name: String): Option[Account] =
-    Some(Account(name)).filter(_.name.length <= 15)
+  def createAccount(name: String): Option[Account] = Some(Account(name)).filter(_.name.length <= 15).filter(!_.name.isBlank)
 }
