c8277c42eea8a275f557087829238e940040baa6
ugenereau <ugo.genereau@epitech.eu>
2021-11-22 16:36:44 +0100

    call api

--- applications/training-api/src/main/scala/routes/BankRoutes.scala
+++ applications/training-api/src/main/scala/routes/BankRoutes.scala
@@ -5,6 +5,7 @@ import routes.input.CreateAccountInput
 
 import cats.effect.{Concurrent, ContextShift, IO, Timer}
 import cats.syntax.semigroupk._
+import fr.fpe.school.api.AccountAPI
 import org.http4s.HttpRoutes
 import sttp.model.StatusCode.Created
 import sttp.tapir.Tapir
@@ -14,10 +15,10 @@ import sttp.tapir.openapi.circe.yaml._
 import sttp.tapir.server.http4s.Http4sServerInterpreter
 import sttp.tapir.swagger.http4s.SwaggerHttp4s
 
-final class BankRoutes()(implicit
-    concurrent: Concurrent[IO],
-    cs: ContextShift[IO],
-    timer: Timer[IO]
+final class BankRoutes(api : AccountAPI)(implicit
+                                         concurrent: Concurrent[IO],
+                                         cs: ContextShift[IO],
+                                         timer: Timer[IO]
 ) extends Tapir
     with TapirJsonCirce {
 
@@ -27,8 +28,7 @@ final class BankRoutes()(implicit
     .in(jsonBody[CreateAccountInput])
     .out(statusCode(Created) and jsonBody[String])
     .description("create an account and return it")
-    .serverLogic[IO](input => IO.pure(input.name).map(Right(_)))
-
+    .serverLogic[IO](input => IO.pure(api.createAccount(input.name)).map(Right(_)))
   private val endpoints = List(createAccount)
 
   private val swaggerRoute =
