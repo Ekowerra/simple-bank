1ffd2418a4a12e563555a5e83e8c968a4ed2764e
Nicolas SANS <nicolas.sansd@gmail.com>
2021-11-22 17:13:36 +0100

    ex 5

--- domain/src/main/scala/api/AccountAPI.scala
+++ domain/src/main/scala/api/AccountAPI.scala
@@ -5,6 +5,6 @@ import model.Account
 
 final class AccountAPI() {
 
-  def createAccount(name: String): Either[String, Account] = ???
+  def createAccount(name: String): Either[String, Account] = Either.cond[String, Account](name.length.<(15).&&(!name.isBlank), (Account(name)), "Invalid username")
 
 }
