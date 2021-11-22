4de2e4adfec663ab57c66aaa3d63a3b419814e39
Baptiste <baptiste.moulard@epitech.eu>
2021-11-22 16:39:58 +0100

    the function is now applied to the http route

--- applications/training-api/src/main/scala/routes/BankRoutes.scala
+++ applications/training-api/src/main/scala/routes/BankRoutes.scala
@@ -5,6 +5,7 @@ import routes.input.CreateAccountInput
 
 import cats.effect.{Concurrent, ContextShift, IO, Timer}
 import cats.syntax.semigroupk._
+import fr.fpe.school.api.AccountAPI
 import org.http4s.HttpRoutes
 import sttp.model.StatusCode.Created
 import sttp.tapir.Tapir
@@ -14,7 +15,7 @@ import sttp.tapir.openapi.circe.yaml._
 import sttp.tapir.server.http4s.Http4sServerInterpreter
 import sttp.tapir.swagger.http4s.SwaggerHttp4s
 
-final class BankRoutes()(implicit
+final class BankRoutes(API: AccountAPI)(implicit
     concurrent: Concurrent[IO],
     cs: ContextShift[IO],
     timer: Timer[IO]
@@ -27,7 +28,7 @@ final class BankRoutes()(implicit
     .in(jsonBody[CreateAccountInput])
     .out(statusCode(Created) and jsonBody[String])
     .description("create an account and return it")
-    .serverLogic[IO](input => IO.pure(input.name).map(Right(_)))
+    .serverLogic[IO](input => IO.pure(API.createAccount(input.name)).map(Right(_)))
 
   private val endpoints = List(createAccount)
 
