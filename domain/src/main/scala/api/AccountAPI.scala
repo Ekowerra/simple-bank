b0e608cbdeed8b3f256f540d74c2f3f0d2b76088
Justin <justin.menard@epitech.eu>
2021-11-22 17:01:20 +0100

    Update bool

--- domain/src/main/scala/api/AccountAPI.scala
+++ domain/src/main/scala/api/AccountAPI.scala
@@ -6,5 +6,5 @@ import model.Account
 final class AccountAPI() {
 
   def createAccount(name: String): Option[Account] =
-    Some(Account(name)).filter(_.name.length <= 15).filter(_.name.trim.isEmpty == false)
+    Some(Account(name)).filter(_.name.length <= 15).filter(_.name.trim.nonEmpty)
 }
