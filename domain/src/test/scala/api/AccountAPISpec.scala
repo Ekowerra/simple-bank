bf49eea1f77a08af99ad7e7cbd63b79056ddc0ed
baptiste <baptiste.renouf@epitech.com>
2021-11-22 17:17:34 +0100

    adding ex3

--- domain/src/test/scala/api/AccountAPISpec.scala
+++ domain/src/test/scala/api/AccountAPISpec.scala
@@ -7,14 +7,14 @@ import org.scalatest.OptionValues
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-class AccountAPISpec extends AnyWordSpec with Matchers with OptionValues {
+class AccountAPISpec extends AnyWordSpec with Matchers {
 
   "AccountApi.createAccount" should {
     "return None if name is incorrect" in {
       val myApi = new AccountAPI()
 
-      myApi.createAccount("a" * 16) shouldBe ???
-      myApi.createAccount("Obiwan") shouldBe ???
+      myApi.createAccount("a" * 16) shouldBe None
+      myApi.createAccount("Obiwan") shouldBe Some(Account("Obiwan"))
     }
   }
 }
