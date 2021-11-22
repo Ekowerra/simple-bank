1aeea9f2e5b0c0ad3f82847bf261711c09ae1e06
Justin <justin.menard@epitech.eu>
2021-11-22 17:17:26 +0100

    Exo 5

--- domain/src/main/scala/api/AccountAPI.scala
+++ domain/src/main/scala/api/AccountAPI.scala
@@ -5,6 +5,8 @@ import model.Account
 
 final class AccountAPI() {
 
-  def createAccount(name: String): Either[String, Account] = ???
+  def createAccount(name: String): Either[String, Account] = {
+    Either.cond[String, Account](name.length < 15 && name.trim.nonEmpty, right = Account(name), left = "Error name")
+  }
 
 }
