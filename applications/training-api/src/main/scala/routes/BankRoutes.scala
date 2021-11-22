b7a70bdfeb87723c51aa371b2a7def72670ef91e
Justin <justin.menard@epitech.eu>
2021-11-22 15:54:43 +0100

    Exo 1

--- applications/training-api/src/main/scala/routes/BankRoutes.scala
+++ applications/training-api/src/main/scala/routes/BankRoutes.scala
@@ -5,6 +5,7 @@ import routes.input.CreateAccountInput
 
 import cats.effect.{Concurrent, ContextShift, IO, Timer}
 import cats.syntax.semigroupk._
+import fr.fpe.school.api.AccountAPI
 import org.http4s.HttpRoutes
 import sttp.model.StatusCode.Created
 import sttp.tapir.Tapir
@@ -27,7 +28,7 @@ final class BankRoutes()(implicit
     .in(jsonBody[CreateAccountInput])
     .out(statusCode(Created) and jsonBody[String])
     .description("create an account and return it")
-    .serverLogic[IO](input => IO.pure(input.name).map(Right(_)))
+    .serverLogic[IO](input => IO.pure(new AccountAPI().createAccount(input.name)).map(Right(_)))
 
   private val endpoints = List(createAccount)
 
