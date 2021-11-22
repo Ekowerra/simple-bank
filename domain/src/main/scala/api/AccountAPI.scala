85266df52504770500abad2018a2f3c277b526c7
mgl <marius.glo@protonmail.com>
2021-11-22 16:12:21 +0100

    Renvoie seulement name

--- domain/src/main/scala/api/AccountAPI.scala
+++ domain/src/main/scala/api/AccountAPI.scala
@@ -2,10 +2,7 @@ package fr.fpe.school
 package api
 
 class AccountAPI {
-  private var accountName = "name"
-
   def createAccount(name: String): String = {
-    accountName = name
-    accountName
+    name
   }
 }
