e6e24ca283e7053171e1c765f4134d314b9e811b
Marin Tostivint <marin.tostivint@epitech.eu>
2021-11-22 16:43:11 +0100

    add exo 2

--- domain/src/main/scala/api/AccountAPI.scala
+++ domain/src/main/scala/api/AccountAPI.scala
@@ -1,7 +1,9 @@
 package fr.fpe.school
 package api
 
+import model.Account
+
 final class AccountAPI() {
 
-  def createAccount(name: String): String = name
+  def createAccount(name: String): Account = Account(name)
 }
