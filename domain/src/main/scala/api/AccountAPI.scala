48681e238b6d454909de4c0d7a2f7f6e8c0925e0
Adrien Ysnel <adrien.ysnel@nickel.eu>
2022-11-09 09:10:09 +0100

    Exercice 5: solution

--- domain/src/main/scala/api/AccountAPI.scala
+++ domain/src/main/scala/api/AccountAPI.scala
@@ -5,6 +5,14 @@ import model.Account
 
 final class AccountAPI() {
 
-  def createAccount(name: String): Either[String, Account] = ???
+  def createAccount(name: String): Either[String, Account] =
+    validateName(name).map(Account(_))
 
+  private def validateName(name: String): Either[String, String] = {
+    val trimmedName = name.trim
+    for {
+      _ <- Either.cond(trimmedName.nonEmpty, (), "name is empty")
+      _ <- Either.cond(trimmedName.length <= 15, (), "name is too long")
+    } yield trimmedName
+  }
 }
