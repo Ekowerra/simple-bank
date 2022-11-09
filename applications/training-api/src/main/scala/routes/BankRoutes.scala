e2c3e5969183f854b9aa2c7ab39229c37593c755
Adrien Ysnel <adrien.ysnel@nickel.eu>
2022-11-09 09:06:44 +0100

    Exercice 2: solution

--- applications/training-api/src/main/scala/routes/BankRoutes.scala
+++ applications/training-api/src/main/scala/routes/BankRoutes.scala
@@ -2,12 +2,14 @@ package fr.fpe.school
 package routes
 
 import api.AccountAPI
+import model.Account
 import routes.input.CreateAccountInput
 
 import cats.effect.{Concurrent, IO}
 import org.http4s.HttpRoutes
 import sttp.model.StatusCode.Created
 import sttp.tapir.Tapir
+import sttp.tapir.generic.auto.schemaForCaseClass
 import sttp.tapir.json.circe.TapirJsonCirce
 import sttp.tapir.server.ServerEndpoint
 import sttp.tapir.server.http4s.Http4sServerInterpreter
@@ -22,7 +24,7 @@ final class BankRoutes(accountAPI: AccountAPI)(implicit
     .in("accounts")
     .post
     .in(jsonBody[CreateAccountInput])
-    .out(statusCode(Created) and jsonBody[String])
+    .out(statusCode(Created) and jsonBody[Account])
     .description("create an account and return it")
     .serverLogic[IO](input => IO.pure(accountAPI.createAccount(input.name)).map(Right(_)))
 
---
+++ domain/src/main/scala/model/Account.scala
@@ -0,0 +1,14 @@
+package fr.fpe.school
+package model
+
+import io.circe.Codec
+import io.circe.generic.semiauto.deriveCodec
+
+import java.time.OffsetDateTime
+import java.util.UUID
+
+final case class Account(name: String)
+
+object Account {
+  implicit val codec: Codec[Account] = deriveCodec
+}
